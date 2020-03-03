424d9a78bdafd71b72ee2855991ba3e867980ad3
mtoffl01 <tofflerm@gmail.com>
2020-03-03 11:44:59 -0500

    fixed a missing period in /api/index.js when calling products file, and also created a products store with its thunk and reducer for getting all products

---
+++ client/store/products.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+//Action types
+const GET_PRODUCTS = 'GET_PRODUCTS'
+
+const initialState = {
+  allProducts: null,
+  singleProduct: null
+}
+
+//Action creators
+const getProducts = allProducts => {
+  return {
+    type: GET_PRODUCTS,
+    allProducts
+  }
+}
+
+//Thunks
+export const fetchProducts = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/products')
+    dispatch(getProducts(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+//Reducer
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return {
+        ...state,
+        allProducts: action.allProducts
+      }
+    default:
+      return state
+  }
+}
