3011db3c02457112a7106e1324f5c6ef72a85dfd
Nadia Sultana <ns_meghna@yahoo.com>
2020-03-04 10:57:07 -0500

    database seeded and magic methods not used in every case in seed file

--- script/seed.js
+++ script/seed.js
@@ -64,6 +64,8 @@ const products = [
 
 const orders = [
   {
+    userId: 1,
+    productId: 1,
     quantity: 1,
     price: 100,
     shipping_Date: new Date(2020, 8, 19),
@@ -71,6 +73,8 @@ const orders = [
     shipping_address: 'Hanover street, NewYork'
   },
   {
+    userId: 2,
+    productId: 2,
     quantity: 2,
     price: 200,
     shipping_Date: new Date(2020, 8, 19),
@@ -79,6 +83,8 @@ const orders = [
   },
 
   {
+    userId: 3,
+    productId: 3,
     quantity: 2,
     price: 300,
     shipping_Date: new Date(2020, 8, 19),
@@ -148,30 +154,39 @@ const seed = async () => {
     const transaction3 = allTransactions[2]
 
     //
-    await user1.addProduct(product1)
-    await user2.addProduct(product3)
-    await user2.addProduct(product1)
-    await user3.addProduct(product3)
-
-    // await user1.addOrder(order1)
-    // await user2.addOrder(order2)
-    // await user3.addOrder(order3)
+    // await user1.addProduct(product1,{quantity: 1,
+    //   price: 100,
+    //   shipping_Date: new Date(2020, 8, 19),
+    //   delivery_Date: new Date(2020, 9, 29),
+    //   shipping_address: 'Hanover street, NewYork'})
+    // await user2.addProduct(product3)
+    // await user2.addProduct(product1)
+    // await user3.addProduct(product3)
 
     //
-    await product1.addUser(user1)
-    await product3.addUser(user2)
-    await product1.addUser(user2)
-    await product3.addUser(user3)
-
-    // await product1.addOrder(order1)
-    // await product1.addOrder(order2)
-    // await product3.addOrder(order2)
-    // await product3.addOrder(order3)
+    //
+    //await product1.addUser(user1)
+    // await product3.addUser(user2)
+    // await product1.addUser(user2)
+    // await product3.addUser(user3)
 
     //
     await transaction1.addOrder(order1)
     await transaction2.addOrder(order2)
     await transaction3.addOrder(order3)
+
+    await transaction1.setUser(user1)
+    await transaction2.setUser(user2)
+    await transaction3.setUser(user3)
+
+    // await user1.addOrder(order1)
+    // await user2.addOrder(order2)
+    // await user3.addOrder(order3)
+    //
+    // await product1.addOrder(order1)
+    // await product1.addOrder(order2)
+    // await product3.addOrder(order2)
+    // await product3.addOrder(order3)
   } catch (err) {
     console.log(red(err))
   }
@@ -193,106 +208,3 @@ if (require.main === module) {
       db.close()
     })
 }
-
-// async function seed() {
-//   await db.sync({force: true})
-//   console.log('db synced!')
-//
-//   const allProducts = await Promise.all(
-//     products.map(product => {
-//       return Product.create(product)
-//     })
-//   )
-//
-//   const allUsers = await Promise.all(
-//     users.map(user => {
-//       return User.create(user)
-//     })
-//   )
-//
-//   const allOrders = await Promise.all(
-//     orders.map(order => {
-//       return Order.create(order)
-//     })
-//   )
-//
-//   const allTransactions = await Promise.all(
-//     transactions.map(transaction => {
-//       return Transaction.create(transaction)
-//     })
-//   )
-//
-//   const product1 = allProducts[0]
-//   const product2 = allProducts[1]
-//   const product3 = allProducts[2]
-//
-//   const user1 = allUsers[0]
-//
-//   const user2 = allUsers[1]
-//   const user3 = allUsers[2]
-//
-//   const order1 = allOrders[0]
-//   const order2 = allOrders[1]
-//   const order3 = allOrders[2]
-//
-//   const transaction1 = allTransactions[0]
-//   const transaction2 = allTransactions[1]
-//   const transaction3 = allTransactions[2]
-//
-//   await user1.addProduct(product1)
-//
-//   await user2.addProduct(product3)
-//   await user2.addProduct(product1)
-//
-//   await user3.addProduct(product3)
-//
-//   await user1.addOrder(order1)
-//
-//   await user2.addOrder(order2)
-//   await user3.addOrder(order3)
-//
-//   // await product1.addUser(user1)
-//   // await product3.addUser(user2)
-//   // await product1.addUser(user2)
-//   //
-//   // await product3.addUser(user3)
-//
-//   await transaction1.addUser(user1)
-//   await transaction2.addUser(user2)
-//   await transaction3.addUser(user3)
-//
-//   await transaction1.addOrder(order1)
-//   await transaction2.addOrder(order2)
-//   await transaction3.addOrder(order3)
-//
-//
-//   console.log(`seeded ${users.length} users`)
-//   console.log(`seeded successfully`)
-// }
-//
-// // We've separated the `seed` function from the `runSeed` function.
-// // This way we can isolate the error handling and exit trapping.
-// // The `seed` function is concerned only with modifying the database.
-// async function runSeed() {
-//   console.log('seeding...')
-//   try {
-//     await seed()
-//   } catch (err) {
-//     console.error(err)
-//     process.exitCode = 1
-//   } finally {
-//     console.log('closing db connection')
-//     await db.close()
-//     console.log('db connection closed')
-//   }
-// }
-//
-// // Execute the `seed` function, IF we ran this module directly (`node seed`).
-// // `Async` functions always return a promise, so we can use `catch` to handle
-// // any errors that might occur inside of `seed`.
-// if (module === require.main) {
-//   runSeed()
-// }
-//
-// // we export the seed function for testing purposes (see `./seed.spec.js`)
-// module.exports = seed
