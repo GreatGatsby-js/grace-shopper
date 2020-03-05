5903c9ca22f5f124c6d443675f1a00d4e74a6220
Tiffany Ma <yunshin.ma@gmail.com>
2020-03-05 13:18:09 -0500

    test: created Product testspec

---
+++ server/db/models/product.spec.js
@@ -0,0 +1,32 @@
+/* global describe beforeEach it */
+
+const db = require('../index')
+const {expect} = require('chai')
+const Product = db.model('product')
+
+describe('Product model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('Product Created', () => {
+    beforeEach(async () => {
+      await Product.create({
+        name: 'batman duck',
+        description: 'I AM BATMAN!!!!! and i am not cheap',
+        price: 500,
+        imageUrl:
+          'https://static1.squarespace.com/static/53208ff6e4b00fbb0f1c2d65/533088f1e4b017ad58855af2/5c69ca397817f7d0df538357/1550437388826/'
+      })
+    })
+
+    it('expects testDuck to be created', async () => {
+      const batmanDuck = await Product.findOne({where: {name: 'batman duck'}})
+      expect(batmanDuck.name).to.be.equal('batman duck')
+    })
+  }) // end describe('product create and delete')
+
+  describe('instanceMethods', () => {
+    //instance method tests here if any
+  }) // end describe('instanceMethods')
+}) // end describe('Product model')
