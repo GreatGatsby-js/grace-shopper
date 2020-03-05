f2facdd2a5cd3d92d666a879abdfd5b3b25da506
Tiffany Ma <yunshin.ma@gmail.com>
2020-03-05 18:24:54 -0500

    feat: model-product editName

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,4 +1,3 @@
-const crypto = require('crypto')
 const Sequelize = require('sequelize')
 const db = require('../db')
 
@@ -21,5 +20,19 @@ const Product = db.define('product', {
       'https://images.homedepot-static.com/productImages/8cbda3d4-6af8-47fb-873c-d1ad13f0c29d/svn/yellow-novelty-place-pool-toys-np-rubberduck-family-6pc-64_1000.jpg'
   }
 })
+
+Product.prototype.editName = async function(newName) {
+  this.name = newName
+  await this.save()
+}
+
+//        delete doesn't work
+// Product.prototype.delete = async function () {
+//   // console.log("product.remove this", this.name)
+//   await this.destroy({
+//     where: { name: this.name}});
+//   // await this.save();
+//   console.log("should've been destroyed", this.name)
+// }
 
 module.exports = Product
