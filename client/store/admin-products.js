40a76cc20f624277365da564de7e799e4172fba4
Tiffany Ma <yunshin.ma@gmail.com>
2020-03-09 22:50:06 -0400

    feat: redux admin-products

--- client/store/admin-products.js
+++ client/store/admin-products.js
@@ -2,30 +2,62 @@ import axios from 'axios'
 
 /*  ACTION TYPES   */
 const ADMIN_GET_ALL_PRODUCTS = 'ADMIN_GET_ALL_PRODUCTS'
+const ADMIN_ADD_PRODUCT = 'ADMIN_ADD_PRODUCT'
 
-/**
- * INITIAL STATE
- */
+/*  INITIAL STATE  */
 const initialState = {
   product: {},
   allProducts: []
 }
 
 /*  ACTION CREATORS  */
-
-const gotAllProducts = allProducts => {
+const gotProducts = allProducts => {
   return {
     type: ADMIN_GET_ALL_PRODUCTS,
     allProducts
   }
 }
 
-/*  THUNK CREATORS   */
+const addProject = newProd => ({
+  type: ADMIN_ADD_PRODUCT,
+  newProd
+})
 
+/*  THUNK CREATORS   */
 export const fetchProducts = () => async dispatch => {
   try {
     const {data} = await axios.get('/api/products')
-    dispatch(gotAllProducts(data))
+    dispatch(gotProducts(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const addToProjects = () => async dispatch => {
+  try {
+    const {data} = await axios.post('/api/products')
+    dispatch(addProject(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const editThisProject = prod => async dispatch => {
+  try {
+    await axios.put(`/api/products/${prod.id}`)
+
+    const {data} = await axios.get('/api/products')
+    dispatch(gotProducts(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const removeFromProducts = product => async dispatch => {
+  try {
+    await axios.delete(`/api/products/${product.id}`)
+    const {data} = await axios.get('/api/products')
+    dispatch(gotProducts(data))
   } catch (err) {
     console.log(err)
   }
@@ -38,6 +70,12 @@ export default function(state = initialState, action) {
       return {
         ...state,
         allProducts: [...action.allProducts]
+      }
+
+    case ADMIN_ADD_PRODUCT:
+      return {
+        ...state,
+        allProducts: [...state.allProducts, action.newProd]
       }
 
     default:
